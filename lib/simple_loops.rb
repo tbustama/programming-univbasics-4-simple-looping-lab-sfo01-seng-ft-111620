b8399863b384d8b87ff87c62ab19ae07eb6cb4b9
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-02 22:50:35 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,28 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, i)
+  count = 0 
+  while count < i do 
+    puts string
+    count += 1
+  end
+end
+
+def output_array(array)
+  array.each do |ele|
+    puts ele 
+  end
+end
+
+def return_string_array(array)
+  new_arr = []
+  array.each do |ele|
+    new_arr << ele.to_s 
+  end
+  return new_arr
+end
