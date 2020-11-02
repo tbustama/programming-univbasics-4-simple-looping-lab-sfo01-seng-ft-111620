86f02a165138ce4f9b2d7f6b6a37e1771ea40e91
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-11-02 22:47:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+def loop_message_five_times(string)
+  4.times do
+    puts string
+  end
+end
+
+def loop_message_n_times(string, i)
+  count = 0 
+  while count < n do 
+    puts string
+  end
+end
+
+def output_array(array)
+  arr.each do |ele|
+    puts ele 
+  end
+end
+
+def return_string_array(array)
+  new_arr = []
+  array.each do |ele|
+    new_arr << ele.to_s 
+  end
+  return new_arr
+end
